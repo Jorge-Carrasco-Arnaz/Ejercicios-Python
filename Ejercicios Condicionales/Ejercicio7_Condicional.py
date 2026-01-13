15b6456d179a2d8dba653b294657360ad3f92d65
Carrasco Arnaz <jcararn@cj.local@2GS-13.cj.local>
2026-01-13 10:36:58 +0100

    Ejercicio 7

--- Ejercicios Condicionales/Ejercicio7_Condicional.py
+++ Ejercicios Condicionales/Ejercicio7_Condicional.py
@@ -3,3 +3,14 @@
 # Entrada: La edad y los ingresos mensuales del usuario
 # Salida: Un mensaje indicando si el usuario tiene que tributar o no
 
+edad = int(input("Introduzca su edad: ")) # Pide al usuario que introduzca su edad
+ingresos= float(input("Introduzca sus ingresos mensuales: ")) # Pide al usuario que introduzca sus ingresos mensuales
+
+if edad >= 16 and ingresos >= 1000: # Comprueba si la edad es mayor o igual a 16 años y si los ingresos son iguales o mayores a 1000€
+    print ("Usted DEBE tributar (El gobierno esta muy FELIZ 😁)") # Imprime un mensaje si las condiciones de edad y ingresos se cumplen
+else: # Si alguna de las dos condiciones no se cumple
+    print ("Usted no debe tributar (Gobierno triste 💔)") # Imprime un mensaje si alguna de las dos condiciones no se cumple
+
+# Realizado por: Jorge Carrasco Arnaz
+# Fecha: 13/01/2026 
+# Hora: 10:34
